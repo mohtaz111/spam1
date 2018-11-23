c414190bd9545d5f33ffe5f30fca4f262715d9be
mohtaz111 <45293606+mohtaz111@users.noreply.github.com>
2018-11-23 20:56:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Credit !spam , Frame , Credit By Moataz , Farm Credit By MoatazMoatazMoatazMoatazMoatazMoatazMoatazMoatazMoatazMoatazMoataz, ez Farm MoatazMoatazMoatazMoatazMoatazMoatazMoatazMoatazMoatazMoatazMoataz - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
